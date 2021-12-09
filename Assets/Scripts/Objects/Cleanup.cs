419450b202497edc5ffcf3e221610c15b5063dd7
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-12-09 16:22:07 +0300

    Create Cleanup.cs

---
+++ Assets/Scripts/Objects/Cleanup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Cleanup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Foam") || other.name.Equals("Reaction"))
+            Destroy(other.gameObject);
+    }
+}
