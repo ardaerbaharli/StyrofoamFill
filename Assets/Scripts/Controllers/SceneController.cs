e316228c270c5f3a1be20d451541519479ce6520
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-12-09 16:21:59 +0300

    Create SceneController.cs

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadGameScreen()
+    {
+        StartCoroutine(LoadAsyncScene("Game"));
+    }
+
+    public void LoadMainScreen()
+    {
+        StartCoroutine(LoadAsyncScene("Main"));
+    }
+
+    public IEnumerator LoadAsyncScene(string sceneName)
+    {
+        System.GC.Collect();
+        Resources.UnloadUnusedAssets();
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/Objects/Reaction.cs
+++ Assets/Scripts/Objects/Reaction.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Reaction : MonoBehaviour
 {
-   
+    void Update()
+    {
+        if (GetComponent<RectTransform>().position.y < -100)
+            Destroy(gameObject);
+    }
 }
